0726a50e6876242298f44c564f86a391041f590d
administrator <koch.richard@schule-thueringen.de>
2025-12-15 10:24:56 +0100

    Bug Fix Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,15 +5,9 @@
 
 package main;
 
-import assets.actors.Tower;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
-        
-        Tower p = new Tower(0, 0, null, "Peter");
-        System.out.println(p.getName());
-        p.setName("fghjklö");
-        System.out.println(p.getName());
     }
 }
